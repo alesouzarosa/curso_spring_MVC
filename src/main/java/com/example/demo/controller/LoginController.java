98872c504c8b9ed72d8928ea80cbd1a9eacad92f
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-02-14 21:34:52 -0300

    commit

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,2 +1,19 @@
-package com.example.demo.controller;public class LoginController {
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/home";
+    }
 }
